65d890a917ddb42297b57ccfd9ca0282e0d7d37b
CARLOS ALBERTO VIVANCO RODRIGUEZ <SISTEMAS.CAVR@GMAIL.COM>
2023-04-10 17:44:15 -0500

    segundo

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,28 +1,9 @@
-// import { useEffect, useState } from "react";
 import { useQuery } from "@apollo/client";
 import {GET_REPOSITORIES} from '../graphql/queries'
-// import axios from "axios";
 
 const useRepositories=()=>{
-    // const [repositories, setRepositories]= useState(null)
-
     const {data={}, loading, refetch} = useQuery(GET_REPOSITORIES);
     const {repositories=null}=data;
-
-
-    // const fetchRepositories = async ()=>{
-    //     try {
-    //         const response = (await axios.get('http://192.168.18.15:5000/api/repositories')).data;
-    //         setRepositories(response);
-    //     } catch (error) {
-    //         console.log(error)
-    //     }
-    // };
-
-    // useEffect(()=>{
-    //     fetchRepositories()
-    // },[]);
-
     const repositoriesNodes = repositories
         ? repositories.edges.map(edge=>edge.node)
         :[];
